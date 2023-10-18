6171d3aa6378ad2c2f6c097e1d6db4dc024730c8
bijin-s <127382944+bijin-s@users.noreply.github.com>
2023-10-18 23:32:45 +0530

    dvc addition

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -62,7 +62,7 @@ class ConfigurationManager:
         training = self.config.training
         prepare_base_model = self.config.prepare_base_model
         params = self.params
-        training_data = os.path.join(self.config.data_ingestion.unzip_dir, "kidney-ct-scan-image")
+        training_data = os.path.join(self.config.data_ingestion.unzip_dir, "10_food_classes_10_percent\\test")
         create_directories([
             Path(training.root_dir)
         ])
